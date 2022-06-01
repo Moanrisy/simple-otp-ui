fd5f5bacb7e8758bea701752b25f74e3a9e28683
moanrisy <moanrisy@gmail.com>
2022-06-01 21:05:13 +0700

    Add clickEvent and press function

---
+++ main.js
@@ -0,0 +1,21 @@
+const firstInput = document.getElementById("first")
+const userInput = ["first", "second", "third", "fourth", "fifth", "sixth"];
+let currentElement = 0;
+
+firstInput.focus()
+
+function clickEvent(first, last) {
+    if (first.value.length) {
+        document.getElementById(last).focus();
+    }
+}
+
+function press(number) {
+    var currentInput = document.getElementById(userInput[currentElement])
+    currentInput.value = number;
+
+    if (currentElement < 5) {
+        currentElement+=1
+        document.getElementById(userInput[currentElement]).focus()
+    }
+}
