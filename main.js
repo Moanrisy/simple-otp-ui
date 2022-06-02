0e840acf5b77820dd3b26be953844255f01266af
moanrisy <moanrisy@gmail.com>
2022-06-03 00:40:04 +0700

    Add input phone number

--- main.js
+++ main.js
@@ -3,14 +3,24 @@ const userInput = ["first", "second", "third", "fourth", "fifth", "sixth"];
 const resendOtpBtn = document.getElementById("resendOtp-btn");
 const backBtn = document.getElementById("back-btn");
 const content = document.querySelector(".content")
+const inputNumber = document.querySelector(".input-number")
 
 let currentElement = 0;
 
 firstInput.focus();
 
+// add space for phone number
+document.getElementById("telphone").addEventListener("keyup", function () {
+  let phoneNumber = this.value;
+  if (phoneNumber.length == 4 || phoneNumber.length == 9) {
+    this.value=this.value+ " ";
+  }
+})
+
 backBtn.addEventListener("click", () => {
   alert("back button clicked");
   content.classList.remove("active")
+  inputNumber.classList.add("active")
 });
 
 resendOtpBtn.addEventListener("click", () => {
